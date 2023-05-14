05adadbd5257c0d02e3b9f9792c930c021e62ebc
bhendi-boi <bhendiisgreen@gmail.com>
2023-05-14 21:39:00 +0530

    added diabled styles for ChooseBase

--- app/ChooseBase.tsx
+++ app/ChooseBase.tsx
@@ -8,25 +8,35 @@ import { Bases } from "@/types";
 type Props = {
   label: string;
   state: Bases;
+  otherState?: Bases;
   setState: React.Dispatch<SetStateAction<Bases>>;
 };
 
-export default function ChooseBase({ label, state, setState }: Props) {
+export default function ChooseBase({
+  label,
+  state,
+  otherState,
+  setState,
+}: Props) {
   return (
     <RadioGroup className="mb-4" value={state} onChange={setState}>
       <RadioGroup.Label className="px-2">{label}</RadioGroup.Label>
       <div className="h-16 overflow-hidden">
         <div className="flex h-20 gap-4 py-3 pb-4 my-4 overflow-x-auto">
-          <RadioGroup.Option value="bin">
-            {({ checked }) => (
+          <RadioGroup.Option disabled={otherState === "bin"} value="bin">
+            {({ checked, disabled }) => (
               <motion.span
                 initial={{
                   backgroundColor: "#ffffff",
                   color: "#111827",
                 }}
                 animate={{
-                  backgroundColor: checked ? "rgb(37 99 235)" : "#ffffff",
-                  color: checked ? "#ffffff" : "#111827",
+                  backgroundColor: disabled
+                    ? "#6b7280"
+                    : checked
+                    ? "rgb(37 99 235)"
+                    : "#ffffff",
+                  color: disabled ? "#f3f3f3" : checked ? "#ffffff" : "#111827",
                 }}
                 className={clsx("px-6 py-3 rounded-full cursor-pointer")}
               >
@@ -34,16 +44,20 @@ export default function ChooseBase({ label, state, setState }: Props) {
               </motion.span>
             )}
           </RadioGroup.Option>
-          <RadioGroup.Option value="oct">
-            {({ checked }) => (
+          <RadioGroup.Option disabled={otherState === "oct"} value="oct">
+            {({ checked, disabled }) => (
               <motion.span
                 initial={{
                   backgroundColor: "#ffffff",
                   color: "#111827",
                 }}
                 animate={{
-                  backgroundColor: checked ? "rgb(37 99 235)" : "#ffffff",
-                  color: checked ? "#ffffff" : "#111827",
+                  backgroundColor: disabled
+                    ? "#6b7280"
+                    : checked
+                    ? "rgb(37 99 235)"
+                    : "#ffffff",
+                  color: disabled ? "#f3f3f3" : checked ? "#ffffff" : "#111827",
                 }}
                 className={clsx("px-6 py-3 rounded-full cursor-pointer")}
               >
@@ -51,16 +65,20 @@ export default function ChooseBase({ label, state, setState }: Props) {
               </motion.span>
             )}
           </RadioGroup.Option>
-          <RadioGroup.Option value="dec">
-            {({ checked }) => (
+          <RadioGroup.Option disabled={otherState === "dec"} value="dec">
+            {({ checked, disabled }) => (
               <motion.span
                 initial={{
                   backgroundColor: "#ffffff",
                   color: "#111827",
                 }}
                 animate={{
-                  backgroundColor: checked ? "rgb(37 99 235)" : "#ffffff",
-                  color: checked ? "#ffffff" : "#111827",
+                  backgroundColor: disabled
+                    ? "#6b7280"
+                    : checked
+                    ? "rgb(37 99 235)"
+                    : "#ffffff",
+                  color: disabled ? "#f3f3f3" : checked ? "#ffffff" : "#111827",
                 }}
                 className={clsx("px-6 py-3 rounded-full cursor-pointer")}
               >
@@ -68,16 +86,20 @@ export default function ChooseBase({ label, state, setState }: Props) {
               </motion.span>
             )}
           </RadioGroup.Option>
-          <RadioGroup.Option value="hex">
-            {({ checked }) => (
+          <RadioGroup.Option disabled={otherState === "hex"} value="hex">
+            {({ checked, disabled }) => (
               <motion.span
                 initial={{
                   backgroundColor: "#ffffff",
                   color: "#111827",
                 }}
                 animate={{
-                  backgroundColor: checked ? "rgb(37 99 235)" : "#ffffff",
-                  color: checked ? "#ffffff" : "#111827",
+                  backgroundColor: disabled
+                    ? "#6b7280"
+                    : checked
+                    ? "rgb(37 99 235)"
+                    : "#ffffff",
+                  color: disabled ? "#f3f3f3" : checked ? "#ffffff" : "#111827",
                 }}
                 className={clsx("px-6 py-3 rounded-full cursor-pointer")}
               >

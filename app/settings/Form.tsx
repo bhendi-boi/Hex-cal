51542442838b3b6f82c6d4ac233b1dbbcc59b69e
bhendi-boi <bhendiisgreen@gmail.com>
2023-05-26 19:28:57 +0530

    added save changes functionality

--- app/settings/Form.tsx
+++ app/settings/Form.tsx
@@ -1,30 +1,52 @@
 "use client";
-import React, { ComponentProps, useState } from "react";
+import React, { ComponentProps, FormEvent, useState } from "react";
 import ChooseBase from "../ChooseBase";
 import Switch from "../Switch";
 import { useSettings } from "@/hooks/useSettings";
 import { Bases } from "@/types";
+import Button from "../Button";
 
 const Form = () => {
-  const [settings] = useSettings();
+  const [settings, updateSettings] = useSettings();
   const [defaultFrom, setDefaultFrom] = useState<Bases>(
     settings.defaultFromBase
   );
   const [defaultTo, setDefaultTo] = useState<Bases>(settings.defaultToBase);
   const [showCopyToClipboard, setShowCopyToClipboard] = useState(
-    settings.showCopyToClipBoard
+    settings.showCopyToClipboard
   );
+  const [allowNegativeNumbers, setAllowNegativeNumbers] = useState(
+    settings.allowNegativeNumbers
+  );
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    updateSettings({
+      defaultFromBase: defaultFrom,
+      defaultToBase: defaultTo,
+      showCopyToClipboard,
+      allowNegativeNumbers,
+    });
+  }
+
   return (
-    <form className="px-2 py-4 space-y-4">
+    <form onSubmit={handleSubmit} className="px-2 py-4 space-y-4">
       <fieldset>
         <StyledLegend>All</StyledLegend>
-        {/* toggle for negative numbers and clipboard */}
-        <div className="flex items-center justify-between">
+        <div className="flex items-center justify-between mb-4">
           <p>Show copy to clipboard</p>
           <Switch
             srText="Show Copy to clipboard"
             enabled={showCopyToClipboard}
             setEnabled={setShowCopyToClipboard}
+          />
+        </div>
+        <div className="flex items-center justify-between">
+          <p>Allow the use of negative numbers</p>
+          <Switch
+            srText="Allow negative numbers"
+            enabled={allowNegativeNumbers}
+            setEnabled={setAllowNegativeNumbers}
           />
         </div>
       </fieldset>
@@ -41,6 +63,9 @@ const Form = () => {
           setState={setDefaultTo}
         />
       </fieldset>
+      <Button type="submit" variant="converter">
+        Save Changes
+      </Button>
     </form>
   );
 };

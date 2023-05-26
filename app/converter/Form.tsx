cce736d001117aa5429ccb56ca638777432465c9
bhendi-boi <bhendiisgreen@gmail.com>
2023-05-27 00:44:56 +0530

    added copy to clipboard functionality

--- app/converter/Form.tsx
+++ app/converter/Form.tsx
@@ -2,10 +2,13 @@
 import { useState, ChangeEvent, FormEvent } from "react";
 import Input from "../Input";
 import Button from "../Button";
+import Toast from "../Toast";
 import ChooseBase from "../ChooseBase";
+import { ClipboardIcon } from "@heroicons/react/24/outline";
 import { useSettings } from "@/hooks/useSettings";
 import { generateRegex } from "@/lib/generateRegex";
 import { converter } from "@/lib/converter";
+import { copyToClipboard } from "@/lib/copyToClipboard";
 import { Bases, InputModeTypes } from "@/types";
 
 const Form = () => {
@@ -14,6 +17,7 @@ const Form = () => {
   const [to, setTo] = useState<Bases>(settings.defaultToBase);
   const [number, setNumber] = useState<string>("");
   const [result, setResult] = useState<string>("");
+  const [showStatus, setShowStatus] = useState(false);
 
   const { pattern, inputMode } = generateRegex(from);
 
@@ -36,6 +40,12 @@ const Form = () => {
     const parsedValue = parser(e.target.value) as string;
     setNumber(parsedValue);
   }
+
+  function handleClick() {
+    copyToClipboard(result);
+    setShowStatus(true);
+  }
+
   function handleSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
     const res = converter({ from, to, number });
@@ -43,42 +53,59 @@ const Form = () => {
   }
 
   return (
-    <form onSubmit={(e) => handleSubmit(e)} className="px-2">
-      <ChooseBase
-        label="From"
-        state={from}
-        setState={setFrom}
-        otherState={to}
-      />
-      <ChooseBase label="To" state={to} setState={setTo} otherState={from} />
-      <div className="flex flex-col gap-2">
-        <label htmlFor="number">Number</label>
-        <Input
-          id="number"
-          name="number"
-          required
-          type="text"
-          pattern={pattern}
-          inputMode={inputMode as InputModeTypes}
-          placeholder="Enter number here ..."
-          value={number}
-          onChange={(e) => handleChange(e)}
+    <>
+      <form onSubmit={(e) => handleSubmit(e)} className="px-2">
+        <ChooseBase
+          label="From"
+          state={from}
+          setState={setFrom}
+          otherState={to}
         />
-      </div>
-      <Button
-        variant="converter"
-        type="submit"
-        className="w-full py-3 my-4 font-medium text-white bg-green-600 rounded-md"
-      >
-        Convert
-      </Button>
-      {result !== undefined && (
-        <div>
-          <h2 className="text-xl font-medium">Result</h2>
-          <p className="text-gray-950">{result}</p>
+        <ChooseBase label="To" state={to} setState={setTo} otherState={from} />
+        <div className="flex flex-col gap-2">
+          <label htmlFor="number">Number</label>
+          <Input
+            id="number"
+            name="number"
+            required
+            type="text"
+            pattern={pattern}
+            inputMode={inputMode as InputModeTypes}
+            placeholder="Enter number here ..."
+            value={number}
+            onChange={(e) => handleChange(e)}
+          />
         </div>
+        <Button
+          variant="converter"
+          type="submit"
+          className="w-full py-3 my-4 font-medium text-white bg-green-600 rounded-md"
+        >
+          Convert
+        </Button>
+      </form>
+      {result !== undefined && (
+        <section aria-labelledby="result" className="px-2">
+          <header className="flex items-center justify-between">
+            <h2 id="result" className="text-xl font-medium">
+              Result
+            </h2>
+            <button
+              onClick={handleClick}
+              className="p-2 cursor-pointer hover:bg-gray-100 active:bg-gray-200 rounded-full"
+            >
+              <ClipboardIcon className="w-6 h-6 text-gray-950" />
+            </button>
+          </header>
+          <p className="text-gray-950">{result}</p>
+        </section>
       )}
-    </form>
+      <Toast
+        state={showStatus}
+        setState={setShowStatus}
+        message="Result copied to clipboard 😉"
+      />
+    </>
   );
 };
 

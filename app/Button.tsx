f62b032f29d4898af859fc04d666af57d0ca5f32
bhendi-boi <bhendiisgreen@gmail.com>
2023-06-20 17:00:46 +0530

    changed Button styles

--- app/Button.tsx
+++ app/Button.tsx
@@ -12,7 +12,7 @@ const Button = ({ children, variant, ...rest }: Props) => {
       {...rest}
       className={clsx(
         (variant === "converter" || variant === "calculator") &&
-          "w-full py-3 my-4 font-medium text-white bg-green-500 dark:bg-green-600 rounded-md",
+          "block w-full py-3 my-4 font-medium text-white bg-gray-950 hover:bg-gray-900 active:bg-gray-950 dark:bg-gray-50 dark:hover:bg-gray-100 dark:active:bg-gray-50 dark:text-headingText rounded-md",
         variant === "outline" &&
           "block w-full py-3 text-blue-600 border border-blue-600 rounded-md hover:bg-gray-100 active:bg-gray-50 bg-gray-50 max-w-xs",
         variant === "filled" &&

0d55e7f3d2be3f9c8d4986b2805182f189934624
bhendi-boi <bhendiisgreen@gmail.com>
2023-12-25 13:36:44 +0530

    loading screen fixed

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,9 +2,9 @@ import InnerHeader from "./InnerHeader";
 
 const loading = () => {
   return (
-    <>
+    <div className="max-w-xl p-6 pb-16 mx-auto">
       <InnerHeader title="Loading ..." />
-    </>
+    </div>
   );
 };
 
